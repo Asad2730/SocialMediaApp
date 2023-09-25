8536387b7f75e255cc27a5e1289e27069376f8ad
Asad2730 <asadsajjad777@gmail.com>
2023-09-25 06:26:58 -0700

    ok

--- flutter_app/lib/widgets/ui.dart
+++ flutter_app/lib/widgets/ui.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/utils/themes.dart';
 
-import 'package:flutter/material.dart';
+
 
 Widget inputText({
   required TextEditingController controller,
